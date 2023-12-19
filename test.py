71878445994c7e7a3246cc9d01ca8617db097446
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2023-12-20 00:08:20 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -3,7 +3,18 @@ from pathlib import Path
 import re
 import sys
 
+
 class FileSorter:
+    CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+
+    MAP = {}
+
+    for cirilic, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+        MAP[ord(cirilic)] = latin
+        MAP[ord(cirilic.upper())] = latin.upper()
+        
     def __init__(self, source_folder):
         self.source_folder = source_folder
 
